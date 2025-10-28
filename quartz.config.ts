cec22b80213bb3fe92371de930db447252cb87f2
luckybearbear <35355954+luckybearbear@users.noreply.github.com>
2025-10-28 14:07:45 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Luckybearbear`Blog",
+    pageTitle: "熊熊的博客",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
